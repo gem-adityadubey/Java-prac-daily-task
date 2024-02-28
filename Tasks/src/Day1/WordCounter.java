55d54daef10ba218eca5c55084b1c5947df60bfa
gem-adityadubey <160013051+gem-adityadubey@users.noreply.github.com>
2024-02-28 17:32:56 +0530

    Updated WordCounter

--- Tasks/src/Day1/WordCounter.java
+++ Tasks/src/Day1/WordCounter.java
@@ -2,17 +2,30 @@ package Day1;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
+class WordFrequency implements Comparable<WordFrequency>{
+    String words;
+    int frequency;
+    public WordFrequency(String words,int frequency){
+        this.words = words;
+        this.frequency = frequency;
+    }
+    @Override
+    public int compareTo(WordFrequency o) {
+        if(this.frequency != o.frequency){
+            return Integer.compare(o.frequency,this.frequency);
+        }
+        return this.words.compareTo(o.words);
+    }
+}
 
 public class WordCounter {
     public static void main(String[] args) throws FileNotFoundException {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the name of the text file:");
         String fileName = sc.nextLine();
-        File file = new File("C:\\Java Tasks\\Day1\\Tasks\\src\\"+fileName+".txt");
+        File file = new File("C:\\Java Tasks\\Day1\\Tasks\\src\\"+fileName);
         String fileContent = readFile(fileName,  file);
         if(fileContent != null) {
             Map<String, Integer> wordCount = countWord(fileContent);
@@ -23,8 +36,14 @@ public class WordCounter {
     }
 
     private static void displayResult(Map<String, Integer> wordCount) {
+        List<WordFrequency> wordFrequencyList = new ArrayList<>();
         for(Map.Entry<String,Integer> entry : wordCount.entrySet()){
             System.out.println("Key : " + entry.getKey()+" Value: "+entry.getValue());
+            wordFrequencyList.add(new WordFrequency(entry.getKey(),entry.getValue()));
+        }
+        Collections.sort(wordFrequencyList);
+        for(WordFrequency wordFrequency : wordFrequencyList){
+            System.out.println(wordFrequency.words+" "+wordFrequency.frequency);
         }
     }
 
@@ -43,10 +62,10 @@ public class WordCounter {
         Map<String , Integer> map = new HashMap<>();
         String[] words = fileContent.toLowerCase().split("\\s+");
         for(String word : words){
-          word = word.replaceAll("[^a-zA-Z]","");
-          if(!word.isEmpty()){
-              map.put(word,map.getOrDefault(word,0)+1);
-          }
+            word = word.replaceAll("[^a-zA-Z]","");
+            if(!word.isEmpty()){
+                map.put(word,map.getOrDefault(word,0)+1);
+            }
         }
         return map;
     }
